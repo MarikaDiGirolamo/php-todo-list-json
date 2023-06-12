4606f714b44157be3ff73503671ada45850be7dd
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-06-12 16:20:18 +0200

    Add api.php

---
+++ api.php
@@ -0,0 +1,3 @@
+MILESTONE 2
+Create una seconda pagina api.php. Questa pagina dovrà restituire dei dati in JSON.
+Per farlo create un array di stringhe in PHP, convertitelo in JSON e restituitelo (attenti all'header!)
